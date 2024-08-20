6daf16d0619d82c2181c8e8e5685f6f1090a77e5
trungnt1811 <trungqn.ptit@gmail.com>
2024-08-20 10:05:12 +0700

    feat: implement reward function in Token.sol

--- genomicdao/contracts/Token.sol
+++ genomicdao/contracts/Token.sol
@@ -23,6 +23,8 @@ contract PostCovidStrokePrevention is ERC20, ERC20Burnable, Ownable {
     }
 
     function reward(address to, uint256 riskScore) public onlyOwner {
-        // TODO: Implement this method: Award PCSP to the user based on his/her risk score
+        require(riskScore >= 1 && riskScore <= 4, "No reward for the risk score");
+        uint256 amount = riskScoreToAward[riskScore];
+        _mint(to, amount);
     }
 }
