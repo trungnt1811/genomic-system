5753b6fbeccc767b1cb202b54822183362109105
trungnt1811 <trungqn.ptit@gmail.com>
2024-08-22 10:22:11 +0700

    feat: deploy smart contracts to optimism-sepolia testnet

--- genomicdao/hardhat.config.js
+++ genomicdao/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    "optimism-sepolia": {
+        url: "https://sepolia.optimism.io",
+        chainId: 11155420,
+        accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
